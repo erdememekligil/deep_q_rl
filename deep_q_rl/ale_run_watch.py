a2d8bfc63041b9e845ee2a66fa40cc4883a7904d
erdememekligil <erdememekligil@gmail.com>
2016-05-29 21:20:44 +0300

    Libs & windows related stuff. Also some changes in plot functions.

--- deep_q_rl/ale_run_watch.py
+++ deep_q_rl/ale_run_watch.py
@@ -10,12 +10,14 @@ import sys
 
 
 def run_watch():
-    command = ['./run_nature.py', '--steps-per-epoch', '0',
+    command = [sys.executable, './run_nature.py', '--steps-per-epoch', '0',
                '--test-length', '10000', '--nn-file', sys.argv[1],
                '--display-screen']
 
     if len(sys.argv) > 2:
         command.extend(['--rom', sys.argv[2]])
+    if len(sys.argv) > 3:
+        command.extend(sys.argv[3:])
 
     p1 = subprocess.Popen(command)
 
--- deep_q_rl/plot_filters.py
+++ deep_q_rl/plot_filters.py
@@ -13,6 +13,11 @@ import sys
 import matplotlib.pyplot as plt
 import cPickle
 import lasagne.layers
+from run_nature import Parameters
+from q_network import DeepQLearner
+import q_learner
+from lasagne.layers import dnn
+
 
 net_file = open(sys.argv[1], 'r')
 try:
@@ -23,15 +28,25 @@ except EOFError:
     network = cPickle.load(net_file)
 print network
 q_layers = lasagne.layers.get_all_layers(network.l_out)
-w = q_layers[1].W.get_value()
-count = 1
-for f in range(w.shape[0]):  # filters
-    for c in range(w.shape[1]):  # channels/time-steps
-        plt.subplot(w.shape[0], w.shape[1], count)
-        img = w[f, c, :, :]
-        plt.imshow(img, vmin=img.min(), vmax=img.max(),
-                   interpolation='none', cmap='gray')
-        plt.xticks(())
-        plt.yticks(())
-        count += 1
-plt.show()
+for l in range(1,q_layers.__len__()):
+    # q_layers[l].get_output_shape_for(img)
+    if dnn.Conv2DDNNLayer.__instancecheck__(q_layers[l]):
+        w = q_layers[1].W.get_value()
+        count = 1
+        plt.figure()
+        for f in range(w.shape[0]):  # filters
+            for c in range(w.shape[1]):  # channels/time-steps
+                plt.subplot(w.shape[0], w.shape[1], count)
+                img = w[f, c, :, :]
+                plt.imshow(img, vmin=img.min(), vmax=img.max(),
+                           interpolation='none', cmap='gray')
+                plt.xticks(())
+                plt.yticks(())
+                count += 1
+        # plt.show()
+        plt.savefig("dnn{}.jpg".format(l))
+        plt.cla()
+        plt.clf()
+
+        plt.close()
+        print("Plot {}".format(l))
